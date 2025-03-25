32eb190e0faafcec68cafada535a56ecdbc13b1c
FaheemCHAB <mdfaheem.aitrich@gmail.com>
2025-03-25 19:58:46 +0530

    fix: removed unwanted packages from package.json

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:8080',
+    API_URL: 'https://referral-backend-90vu.onrender.com',
     FRONTEND_URL: 'http://localhost:4200'
 };
