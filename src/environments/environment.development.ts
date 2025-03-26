4bdcc1566e1bcc7d9ab0a22d49a02048b4cfbe09
FaheemCHAB <mdfaheem.aitrich@gmail.com>
2025-03-26 11:41:14 +0530

    changed env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production: false,
-    API_URL: 'https://referral-backend-90vu.onrender.com',
-    FRONTEND_URL: 'http://localhost:4200'
+    API_URL: 'http://localhost:8080',
+    // API_URL: 'https://referral-backend-90vu.onrender.com',
+    FRONTEND_URL: 'https://referral-4zdw.onrender.com/',
+    // FRONTEND_URL: 'http://localhost:4200',
+
 };
