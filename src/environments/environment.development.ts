59e280bd7b59bed20e2d586cdcb0cb00aa325442
FaheemCHAB <mdfaheem.aitrich@gmail.com>
2025-03-27 10:52:40 +0530

    updated frontend url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     API_URL: 'https://referral-backend-90vu.onrender.com',
     // API_URL: 'https://referral-backend-90vu.onrender.com',
-    FRONTEND_URL: 'https://referral-4zdw.onrender.com',
+    FRONTEND_URL: 'https://referral-app.netlify.app',
     // FRONTEND_URL: 'http://localhost:4200',
 
 };
