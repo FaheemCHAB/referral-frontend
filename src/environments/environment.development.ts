0551714e9c6a5862197f63a4c1c2cd24f0007353
FaheemCHAB <mdfaheem.aitrich@gmail.com>
2025-06-25 16:21:34 +0530

    feat : environment files changed

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    API_URL: 'https://referral-backend-90vu.onrender.com',
-    // API_URL: 'http://localhost:8080',
-    FRONTEND_URL: 'https://referral-app.netlify.app',
-    // FRONTEND_URL: 'http://localhost:4200',
-
-};
